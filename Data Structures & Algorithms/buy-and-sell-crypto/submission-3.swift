c208163d8e4f74b80597b75eef273274d2cea440
Ryan Mease <87209624+rmease@users.noreply.github.com>
2026-08-18 11:14:47 -0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.swift
@@ -0,0 +1,32 @@
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        var left = 0
+        var right = 1
+
+        var maxProfit = 0
+
+        guard prices.count > 1 else {
+            return 0
+        }
+
+        while right < prices.count {
+            let leftPrice = prices[left]
+            let rightPrice = prices[right]
+
+            if leftPrice >= rightPrice {
+                left += 1
+
+                // if left and right collide, move right to keep the window open
+                if left == right {
+                    right += 1
+                }
+            } else {
+                let currentProfit = rightPrice - leftPrice
+                maxProfit = max(maxProfit, currentProfit)
+                right += 1
+            }
+        }
+
+        return maxProfit
+    }
+}
