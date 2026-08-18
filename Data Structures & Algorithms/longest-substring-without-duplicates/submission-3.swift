12b68fd94f7ef912dd4672abb1d50e601d3d2327
Ryan Mease <87209624+rmease@users.noreply.github.com>
2026-08-18 11:26:51 -0700

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.swift
@@ -0,0 +1,28 @@
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        // guard s.count > 1 else {
+        //     return s.count
+        // }
+
+        var left = 0
+        var right = 0
+
+        var maxLength = 0
+        var characters: [Character] = Array(s)
+
+        var currentCharacters = Set<Character>()
+
+        while right < characters.count {
+            while currentCharacters.contains(characters[right]) {
+                currentCharacters.remove(characters[left])
+                left += 1
+            }
+
+            currentCharacters.insert(characters[right])
+            maxLength = max(maxLength, (right - left + 1))
+            right += 1
+        }
+
+        return maxLength
+    }
+}
