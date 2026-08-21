8e94d196a30558a0d49fbb0ea2e17f0356d1dd0b
Ryan Mease <87209624+rmease@users.noreply.github.com>
2026-08-21 14:35:38 -0700

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.swift
@@ -0,0 +1,36 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        // set boundary of search scope to left and right ends of the array
+
+        var leftIndex = 0
+        var rightIndex = nums.count - 1
+        var medianIndex = leftIndex + ((rightIndex - leftIndex) / 2)
+
+        // until leftIndex and rightIndex collide, progressively divide the remaining scope
+
+        // if the number at the middle of the scope...
+        // is == target: return median index
+        // is < target:  restrict scope to after median index (set leftIndex to medianIndex + 1)
+        // is > target:  restrict scope to before median index (set rightIndex to medianIndex - 1)
+
+        // this operates in O(logN) -- faster than O(n) -- because we progressively halve the scope
+
+        while leftIndex <= rightIndex {
+            let medianNum = nums[medianIndex]
+
+            if medianNum == target {
+                return medianIndex
+            }
+            if medianNum < target {
+                leftIndex = medianIndex + 1
+                medianIndex = leftIndex + ((rightIndex - leftIndex) / 2)
+            }
+            if medianNum > target {
+                rightIndex = medianIndex - 1
+                medianIndex = leftIndex + ((rightIndex - leftIndex) / 2)
+            }
+        }
+
+        return -1
+    }
+}
