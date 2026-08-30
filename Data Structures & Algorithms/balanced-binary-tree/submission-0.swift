2923193e95631e7d019a464669c1bc43daefe211
Ryan Mease <87209624+rmease@users.noreply.github.com>
2026-08-30 10:40:14 -0700

    Add: balanced-binary-tree - submission-0

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-0.swift
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func isBalanced(_ root: TreeNode?) -> Bool {
+        guard let root else {
+            return true
+        }
+
+        return dfs(root) != -1
+    }
+
+    private func dfs(_ root: TreeNode?) -> Int {
+        guard let root else {
+            return 0
+        }
+
+        let rightDepth = dfs(root.right)
+        if rightDepth == -1 {
+            return -1 // found imbalance in right tree's children
+        }
+
+        let leftDepth = dfs(root.left)
+        if leftDepth == -1 {
+            return -1 // found imbalance in left tree's children
+        }
+
+        // rightDepth and leftDepth must be equal or vary only by 1
+        guard abs(leftDepth - rightDepth) < 2 else {
+            return -1
+        }
+
+        return 1 + max(leftDepth, rightDepth)
+    }
+}
